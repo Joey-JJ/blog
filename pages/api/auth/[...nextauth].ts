3f8f8872f5f339535ef0d4ec8cbc55856da483d1
Joey van der Valk <joeyvandervalk@gmail.com>
2022-09-14 19:43:52 +0200

    Set up basics for prisma and nextauth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [],
+};
+
+export default NextAuth(authOptions);
